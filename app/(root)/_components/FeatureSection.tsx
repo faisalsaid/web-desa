3b534b0083bbc8c6aa4f330164e5f7810d706639
faisalsaid <falsdesain@gmail.com>
2025-08-10 06:26:38 +0900

    feature section

---
+++ app/(root)/_components/FeatureSection.tsx
@@ -0,0 +1,49 @@
+import { FaBuildingColumns } from 'react-icons/fa6';
+import { FaChartBar } from 'react-icons/fa';
+import { IconType } from 'react-icons';
+import { LuNewspaper } from 'react-icons/lu';
+import { TbSpeakerphone } from 'react-icons/tb';
+import { BsBagCheck } from 'react-icons/bs';
+import { LuPackageCheck } from 'react-icons/lu';
+import { PiImages } from 'react-icons/pi';
+import { RiListIndefinite } from 'react-icons/ri';
+
+const menu = [
+  { title: 'Profil Desa', icon: FaBuildingColumns },
+  { title: 'Info Grafis', icon: FaChartBar },
+  { title: 'IDM', icon: RiListIndefinite },
+  { title: 'PPID', icon: LuNewspaper },
+  { title: 'Berita', icon: TbSpeakerphone },
+  { title: 'Belanja', icon: BsBagCheck },
+  { title: 'Bansos', icon: LuPackageCheck },
+  { title: 'Galeri', icon: PiImages },
+];
+
+const FeatureSection = () => {
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      {menu.map((item) => (
+        <FetureCard key={item.title} title={item.title} icon={item.icon} />
+      ))}
+    </div>
+  );
+};
+
+export default FeatureSection;
+
+interface FeatureCardProps {
+  title: string;
+  icon: IconType;
+}
+
+const FetureCard = ({ title, icon }: FeatureCardProps) => {
+  const Icon = icon;
+  return (
+    <div className="flex items-center flex-col space-y-2">
+      <div className="h-16 aspect-square bg-amber-500 rounded-xl flex items-center justify-center text-white">
+        <Icon size={30} />
+      </div>
+      <p className="text-xs">{title}</p>
+    </div>
+  );
+};
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
+import FeatureSection from './_components/FeatureSection';
 import HeroSection from './_components/HeroSection';
 
 export default function Home() {
   return (
-    <div className="p-4">
+    <div className="p-4 space-y-8">
       <HeroSection />
+      <FeatureSection />
     </div>
   );
 }

10625e4f6764c1fc07afe789d21fd7b1b5202b00
faisalsaid <falsdesain@gmail.com>
2025-08-11 11:25:24 +0900

    tour section ui

--- app/(root)/_components/TourSection.tsx
+++ app/(root)/_components/TourSection.tsx
@@ -1,5 +1,80 @@
+'use client';
+
+// import Swiper core and required modules
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+} from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+import Link from 'next/link';
+
 const TourSection = () => {
-  return <div>TourSection</div>;
+  return (
+    <div className="space-y-4">
+      <div className="space-y-2 text-center">
+        <div className=" text-2xl font-semibold text-amber-600">Pariwisata</div>
+        <p>
+          Layanan yang mempermudah promosi wisata Desa sehingga dapat menarik
+          pengunjung Desa
+        </p>
+      </div>
+      <div className="">
+        <Swiper
+          // install Swiper modules
+          modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+          spaceBetween={0}
+          slidesPerView={'auto'}
+          loop={true}
+          //   navigation
+          pagination={{ clickable: true }}
+          //   scrollbar={{ draggable: true }}
+          //   onSwiper={(swiper) => console.log(swiper)}
+          //   onSlideChange={() => console.log('slide change')}
+          speed={5000}
+          autoplay={{
+            delay: 0,
+            disableOnInteraction: false,
+          }}
+          freeMode={{
+            enabled: true,
+            momentum: false,
+          }}
+        >
+          {Array.from({ length: 6 }, (_, i) => (
+            <SwiperSlide key={i}>
+              <TourCard />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      <div className="flex items-center justify-end text-sm">
+        <Link className="text-muted-foreground" href={'/'}>
+          See All
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default TourSection;
+
+const TourCard = () => {
+  return (
+    <div className="w-80 rounded-xl overflow-hidden bg-muted">
+      <div className="bg-amber-600 w-full h-56 flex items-center justify-center rounded-xl">
+        Image
+      </div>
+    </div>
+  );
+};

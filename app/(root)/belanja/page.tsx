1a8aa402de740b2b6aedaad71947d86d4c3fa312
faisalsaid <falsdesain@gmail.com>
2025-08-14 11:39:19 +0900

    product detail page

--- app/(root)/belanja/page.tsx
+++ app/(root)/belanja/page.tsx
@@ -1,5 +1,6 @@
 import { Star, Tag } from 'lucide-react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const dummyProducts = [
   {
@@ -41,7 +42,7 @@ const BelanjaPage = () => {
         </p>
       </div>
 
-      <div className="space-y-4">
+      <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
         {dummyProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
@@ -66,34 +67,36 @@ interface ProductCardProps {
 
 const ProductCard = ({ product }: ProductCardProps) => {
   return (
-    <div className="flex   bg-muted rounded-lg shadow overflow-hidden items-stretch">
-      <div className="w-1/3 relative rounded-lg overflow-hidden bg-amber-500 ">
-        <Image
-          alt="product"
-          src={
-            'https://images.unsplash.com/photo-1509440159596-0249088772ff?q=80&w=1172&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-          }
-          fill
-          className="object-cover w-full h-full"
-        />
-      </div>
-      <div className="w-2/3 space-y-2 p-4">
-        <h5>{product.title}</h5>
-        <div className="text-xs flex items-center gap-1 text-muted-foreground">
-          <Tag className="w-3 h-3" /> <span> {product.tag}</span>
-        </div>
-        <div className="flex items-center justify-between">
-          <div className="flex  items-center text-muted-foreground">
-            <Star size={14} />
-            <Star size={14} />
-            <Star size={14} />
-            <Star size={14} />
-            <Star size={14} />
+    <div>
+      <Link href={`/belanja/${product.id}`}>
+        <div className="flex   bg-muted rounded-lg shadow overflow-hidden items-stretch">
+          <div className="w-1/3 relative rounded-lg overflow-hidden bg-amber-500 ">
+            <Image
+              alt="product"
+              src={product.image}
+              fill
+              className="object-cover w-full h-full"
+            />
           </div>
+          <div className="w-2/3 space-y-2 p-4">
+            <h5>{product.title}</h5>
+            <div className="text-xs flex items-center gap-1 text-muted-foreground">
+              <Tag className="w-3 h-3" /> <span> {product.tag}</span>
+            </div>
+            <div className="flex items-center justify-between">
+              <div className="flex  items-center text-muted-foreground">
+                <Star size={14} />
+                <Star size={14} />
+                <Star size={14} />
+                <Star size={14} />
+                <Star size={14} />
+              </div>
 
-          <div>Rp. {product.price}</div>
+              <div>Rp. {product.price}</div>
+            </div>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };

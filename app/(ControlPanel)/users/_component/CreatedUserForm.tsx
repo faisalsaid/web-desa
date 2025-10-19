b5960e157afe4298aa438cbb0f16eb30a7c13618
faisalsaid <falsdesain@gmail.com>
2025-10-19 14:06:27 +0900

    add user form dialog

--- app/(ControlPanel)/users/_component/CreatedUserForm.tsx
+++ app/(ControlPanel)/users/_component/CreatedUserForm.tsx
@@ -1,16 +1,29 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 import { Plus } from 'lucide-react';
 
 const CreatedUserForm = () => {
   return (
-    <div>
-      <Button>
-        <Plus />
-        <span>New User</span>
-      </Button>
-    </div>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>
+          <Plus /> New User
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <h2>Add New User</h2>
+        </DialogHeader>
+        <div>Halo Dialog</div>
+      </DialogContent>
+    </Dialog>
   );
 };
 

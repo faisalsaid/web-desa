9302b9b129279dffc57b724772493f076d3b7775
faisalsaid <falsdesain@gmail.com>
2025-10-22 20:39:59 +0900

    _vercel_jwt

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,8 @@ import type { NextRequest } from 'next/server';
 export function middleware(req: NextRequest) {
   const token =
     req.cookies.get('next-auth.session-token')?.value ||
-    req.cookies.get('__Secure-next-auth.session-token')?.value;
+    req.cookies.get('__Secure-next-auth.session-token')?.value ||
+    req.cookies.get('_vercel_jwt')?.value;
 
   // Jika user belum login dan akses route yang dilindungi
   if (!token && req.nextUrl.pathname.startsWith('/dashboard')) {

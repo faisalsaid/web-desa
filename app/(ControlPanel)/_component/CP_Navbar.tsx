743507ffe216b79c3db8fc24b72a524d3c6f2dff
faisalsaid <falsdesain@gmail.com>
2025-10-23 13:50:30 +0900

    user drop down menu CP navbar

--- app/(ControlPanel)/_component/CP_Navbar.tsx
+++ app/(ControlPanel)/_component/CP_Navbar.tsx
@@ -1,10 +1,22 @@
+import ThemeSwitcher from '@/components/ThemeSwitcher';
 import { SidebarTrigger } from '@/components/ui/sidebar';
+import UserMenu from './UserMenu';
+import { auth } from '@/auth';
 
-const CPNavbar = () => {
+const CPNavbar = async () => {
+  const session = await auth();
   return (
-    <div>
-      <SidebarTrigger />
-    </div>
+    <nav className="p-4 flex items-center justify-between sticky top-0 z-10 bg-primary-foreground">
+      {/* LEFT */}
+      <div className="flex gap-2 items-center">
+        <SidebarTrigger className="" />
+        <ThemeSwitcher />
+      </div>
+      {/* RIGHT */}
+      <div className="flex items-center gap-4">
+        <UserMenu session={session} />
+      </div>
+    </nav>
   );
 };
 
--- app/(ControlPanel)/dashboard/page.tsx
+++ app/(ControlPanel)/dashboard/page.tsx
@@ -1,9 +1,5 @@
-import { auth } from '@/auth';
-
 const DashboardPage = async () => {
-  const session = await auth();
-
-  return <div>Halo {session?.user ? session.user.email : 'Guest'}</div>;
+  return <div>DASHBOARD</div>;
 };
 
 export default DashboardPage;

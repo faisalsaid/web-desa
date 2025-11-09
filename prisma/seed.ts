5305bc29c93781e8a57ab34e2a6dd41ce5cb7ffb
faisalsaid <falsdesain@gmail.com>
2025-11-09 21:38:44 +0900

    seed vilage profile

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,11 +3,13 @@ const prisma = new PrismaClient();
 
 // Import semua seed file
 import seedAdminUser from './seeds/adminUser';
+import seedVillageProfile from './seeds/villageProfile';
 
 async function main() {
   console.log('⏳ Running Seed...');
 
   await seedAdminUser(prisma);
+  await seedVillageProfile(prisma);
 
   console.log('✅ ALL SEED COMPLETED');
 }

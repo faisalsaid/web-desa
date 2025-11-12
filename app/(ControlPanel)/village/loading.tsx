b21a7e9198ecb9abc4739dd77d0f64340b34df3a
faisalsaid <falsdesain@gmail.com>
2025-11-12 14:45:23 +0900

    crete village route

---
+++ app/(ControlPanel)/village/loading.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { Spinner } from '@/components/ui/spinner';
+
+const VillageLoadingPage = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <Spinner />
+    </div>
+  );
+};
+
+export default VillageLoadingPage;

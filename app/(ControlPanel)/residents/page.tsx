a871bb7e8dd398e1fc4ac8bbb13e614fd552d8fe
faisalsaid <falsdesain@gmail.com>
2025-11-13 20:15:10 +0900

    create add new residetn button residet page

--- app/(ControlPanel)/residents/page.tsx
+++ app/(ControlPanel)/residents/page.tsx
@@ -1,5 +1,25 @@
+import { Button } from '@/components/ui/button';
+import ContentCard from '../_component/ContentCard';
+import { UserPlus } from 'lucide-react';
+import Link from 'next/link';
+
 const ResidentsPage = () => {
-  return <div>ResidentsPage</div>;
+  return (
+    <div className="space-y-4">
+      <ContentCard className=" ">
+        <div className="flex gap-4 items-center justify-between">
+          <h2 className="text-xl font-semibold">Data Penduduk</h2>
+          <div>
+            <Link href={'/residents/add'}>
+              <Button>
+                <UserPlus /> <span>Tambah Warga</span>
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </ContentCard>
+    </div>
+  );
 };
 
 export default ResidentsPage;

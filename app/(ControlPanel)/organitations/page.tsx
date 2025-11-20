3b8637274c67ecdad3a008e79549ce8197bb77c2
faisalsaid <falsdesain@gmail.com>
2025-11-20 11:40:17 +0900

    add organitations route

---
+++ app/(ControlPanel)/organitations/page.tsx
@@ -0,0 +1,11 @@
+import ContentCard from '@/app/(ControlPanel)/_component/ContentCard';
+
+export default function OrganitationsPage() {
+  return (
+    <div className="space-y-4">
+      <ContentCard>
+        <h1 className="text-xl font-semibold">Organitations Page</h1>
+      </ContentCard>
+    </div>
+  );
+}

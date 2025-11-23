f85af8ee39387b5418569272200682cbe2c3ffce
faisalsaid <falsdesain@gmail.com>
2025-11-23 15:06:22 +0900

    sincrone next auth

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ export async function middleware(req: NextRequest) {
 
   const token = await getToken({
     req,
-    secret: process.env.AUTH_SECRET,
+    secret: process.env.NEXTAUTH_SECRET,
   });
 
   console.log('MIDLEWARE ', token);

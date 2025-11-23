6d086cc0983f364b907fe784c358ef42b883fccf
faisalsaid <falsdesain@gmail.com>
2025-11-23 21:15:13 +0900

    create accounting/apbdes route

---
+++ app/(ControlPanel)/accounting/apbdes/loading.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { Spinner } from '@/components/ui/spinner';
+
+export default function ApbdesLoading() {
+  return (
+    <div className="flex items-center justify-center">
+      <Spinner />
+    </div>
+  );
+}

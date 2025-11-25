08a6d64688f0662393debf9b91a22febb808579e
faisalsaid <falsdesain@gmail.com>
2025-11-26 06:26:11 +0900

    create route apbdesa/financing

---
+++ app/(ControlPanel)/apbdesa/financing/page.tsx
@@ -0,0 +1,11 @@
+import ContentCard from '@/app/(ControlPanel)/_component/ContentCard';
+
+export default function FinancingPage() {
+  return (
+    <div className="space-y-4">
+      <ContentCard>
+        <h1 className="text-xl font-semibold">Financing Page</h1>
+      </ContentCard>
+    </div>
+  );
+}

011d0c5b10f804de5b29d7d88fec7b661327e00d
faisalsaid <falsdesain@gmail.com>
2025-11-25 23:36:08 +0900

    setting sidebar

--- app/(ControlPanel)/layout.tsx
+++ app/(ControlPanel)/layout.tsx
@@ -1,13 +1,23 @@
 import { AppSidebar } from '@/app/(ControlPanel)/_component/app-sidebar';
 import { SidebarProvider } from '@/components/ui/sidebar';
 import CPNavbar from './_component/CP_Navbar';
+import { cookies } from 'next/headers';
 
 const layout = async ({
   children,
 }: Readonly<{ children: React.ReactNode }>) => {
+  const cookieStore = await cookies();
+  const defaultOpen = cookieStore.get('sidebar_state')?.value === 'true';
   return (
     <>
-      <SidebarProvider>
+      <SidebarProvider
+        defaultOpen={defaultOpen}
+        style={
+          {
+            '--sidebar-width': '12rem',
+          } as React.CSSProperties
+        }
+      >
         <AppSidebar />
         <main className="w-full">
           <CPNavbar />
--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,12 @@ const nextConfig: NextConfig = {
     domains: ['images.unsplash.com', 'source.unsplash.com', 'unsplash.com'],
   },
   /* config options here */
+
+  allowedDevOrigins: [
+    'http://localhost:3000',
+    'http://10.11.148.235:3000',
+    'http://192.168.137.1:3000',
+  ],
 };
 
 export default nextConfig;
